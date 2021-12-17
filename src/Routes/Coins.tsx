ffeb6bcba4da9b69bf91e6307246931b5d7fe0a3
JinKyung Kim <kimradium@naver.com>
2021-12-17 21:37:11 +0900

    CRYPTO TRACKER #3 - Home part One

--- src/Routes/Coins.tsx
+++ src/Routes/Coins.tsx
@@ -1,12 +1,87 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Title = styled.h1`
+  font-size: 48px;
   color: ${(props) => props.theme.accentColor};
 `;
 
+const Container = styled.div`
+  padding: 0px 20px;
+`;
+
+const Header = styled.header`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CoinsList = styled.ul``;
+
+const Coin = styled.li`
+  color: ${(props) => props.theme.textColor};
+  margin-bottom: 10px;
+  font-weight: 700;
+  border-bottom: 1px solid #d7d7d7;
+  a {
+    padding: 20px;
+    transition: color 0.2s ease-in;
+    display: block;
+  }
+  &:hover {
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
+
+const coins = [
+  {
+    id: "btc-bitcoin",
+    name: "Bitcoin",
+    symbol: "BTC",
+    rank: 1,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "eth-ethereum",
+    name: "Ethereum",
+    symbol: "ETH",
+    rank: 2,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "hex-hex",
+    name: "HEX",
+    symbol: "HEX",
+    rank: 3,
+    is_new: false,
+    is_active: true,
+    type: "token",
+  },
+];
+
 const Coins = () => {
-  return <Title>Coins</Title>;
+  return (
+    <Container>
+      <Header>
+        <Title>Coins</Title>
+      </Header>
+      <CoinsList>
+        {coins.map((value) => (
+          <Coin key={value.id}>
+            <Link to={`/${value.id}`}>{value.name} &rarr;</Link>
+          </Coin>
+        ))}
+      </CoinsList>
+    </Container>
+  );
 };
 
 export default Coins;
