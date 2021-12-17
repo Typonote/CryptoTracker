583767399e2981e31b82f286617bbbe290f77e3c
JinKyung Kim <kimradium@naver.com>
2021-12-17 16:15:23 +0900

    #2 Typing the Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,12 @@
 import React from "react";
-import styled, { keyframes } from "styled-components";
-
-const Wrapper = styled.div`
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-  justify-content: center;
-  align-items: center;
-`;
-
-const rotationAnimation = keyframes`
-  0% {
-    transform:rotate(0deg);
-    border-radius:0px;
-  }
-  50% {
-    border-radius:100px;
-  }
-  100%{
-    transform:rotate(360deg);
-    border-radius:0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: ${(props) => props.theme.backgroundColor};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: ${rotationAnimation} 1s linear infinite;
-  ${Emoji}:hover {
-    font-size: 98px;
-  }
-`;
+import Circle from "./Components/Circle";
 
 function App() {
   return (
-    <Wrapper>
-      <Box>
-        <Emoji>🤩</Emoji>
-      </Box>
-      <Emoji>🔥</Emoji>
-    </Wrapper>
+    <div>
+      <Circle bgColor="teal" />
+      <Circle bgColor="tomato" />
+    </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ThemeProvider } from "styled-components";
 import App from "./App";
 
-const darkTheme = {
-  backgroundColor: "#111",
-};
-
-const lightTheme = {
-  backgroundColor: "tomato",
-};
-
 ReactDOM.render(
-  <ThemeProvider theme={darkTheme}>
+  <React.StrictMode>
     <App />
-  </ThemeProvider>,
+  </React.StrictMode>,
+
   document.getElementById("root")
 );
