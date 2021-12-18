2ba988b95a5262930c8895b1823f0670112d41d8
JinKyung Kim <kimradium@naver.com>
2021-12-18 12:24:38 +0900

    CRYPTO TRACKER #7 - Data Types

--- src/Routes/Coin.tsx
+++ src/Routes/Coin.tsx
@@ -31,18 +31,73 @@ interface RouterState {
   };
 }
 
+interface InfoInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface PriceInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
+  };
+}
+
 const Coin = () => {
   const { coinId } = useParams<{ coinId: string }>();
   const { state } = useLocation() as RouterState;
 
-  const [info, setInfo] = useState({});
-  const [price, setPrice] = useState({});
+  const [info, setInfo] = useState<InfoInterface>();
+  const [price, setPrice] = useState<PriceInterface>();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   const GetInfoAPI = async () => {
     setError(null);
-    setInfo({});
+    setInfo(undefined);
     setLoading(true);
     const InfoResponse = await CoinApi.Get_Info(`/${coinId}`)
       .then((response) => {
@@ -58,7 +113,7 @@ const Coin = () => {
 
   const GetPriceAPI = async () => {
     setError(null);
-    setPrice({});
+    setPrice(undefined);
     setLoading(true);
     const PriceResponse = await CoinApi.Get_Price(`/${coinId}`)
       .then((response) => {
